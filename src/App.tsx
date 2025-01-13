1ed54cd117a2d3c5ef20fe6a5621ba398f812162
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2025-01-13 03:01:16 -0600

    3D MODEL

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Suspense, useRef } from 'react'
+import  {Model}  from './Scene'
+import { Environment, OrbitControls } from '@react-three/drei'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cameraRef = useRef<HTMLCanvasElement | null>(null);
+  interface CameraRef {
+    position: {
+      x: number;
+      y: number;
+      z: number;
+    };
+  }
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log("aasdas");
+    if (event.key === 'Enter' && cameraRef.current) {
+      const { position } = cameraRef.current as unknown as CameraRef;
+      console.log(`Camera Position: x=${position.x}, y=${position.y}, z=${position.z}`);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>PORTAFOLIO DE RONALDO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='w-full h-screen relative' onKeyDown={ () => handleKeyDown } >
+      <p className='absolute text-white z-40 text-center w-full font-semibold text-3xl mt-5 font-mono'>Hi my name's Ronaldo Acevedo</p>
+      <Canvas className='w-full h-screen bg-black'
+      camera={{ position: [0,1,-3.4]}}
+      ref={cameraRef}>
+        <OrbitControls/>
+        <Suspense fallback={null}>
+          
+          
+          <mesh
+                rotation={[0,1.632,0]}
+                position={[0,-0.6,-0.9]}
+                >
+                  <Model/>
+                </mesh>
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+          
+        </Suspense>
+       
+      </Canvas>
+    </section>
   )
 }
 
